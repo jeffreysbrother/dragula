0a06733743d2fbdaafec62625345e432c725e23c
James Cool <jeffreysbrother@gmail.com>
2016-10-26 15:52:11 -0700

    clarify issue

--- scripts/example.js
+++ scripts/example.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// var sortable = document.getElementById('sortable');
-
-// THIS IS WHERE COPYING IS ENABLED
 dragula([document.getElementById('left-copy'), document.getElementById('right-copy')], {
-  copy: true,
-  revertOnSpill: true,
-  copySortSource: true
+  copy: true
 })
-    // this will do the color stuff
-// .on('drop', function (el) {
-//   el.className += ' ex-moved';
-// });
